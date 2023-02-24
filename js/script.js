df37298549e033955982af5dbc26dc8ab6c0b162
Umar23A <abduaziz.botirov@mail.ru>
2023-02-24 20:40:41 +0500

    agro

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Smooth scroll blocking
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( 'onwheel' in document ) {
+		window.onwheel = function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'DOMMouseScroll', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	} else if ( 'onmousewheel' in document ) {
+		window.onmousewheel= function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'onmousewheel', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	}
+
+	try { $('body').unmousewheel(); } catch( error ) {}
+});
+
+$(window).load(function(){
+	$('.slider')._TMS({
+		preset:'diagonalExpand',
+		easing:'easeOutQuad',
+		duration:800,
+		pagination:true,
+		slideshow:6000
+	})
+})
